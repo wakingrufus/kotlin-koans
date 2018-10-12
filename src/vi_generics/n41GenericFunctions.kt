82e31d5b62b0f519445d843bc4cd38b76f4f7530
John Burns <wakingrufus@gmail.com>
2018-10-12 14:09:45 -0500

    finish koans

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -4,7 +4,7 @@ import util.TODO
 import java.util.*
 
 fun task41(): Nothing = TODO(
-    """
+        """
         Task41.
         Add a 'partitionTo' function that splits a collection into two collections according to a predicate.
         Uncomment the commented invocations of 'partitionTo' below and make them compile.
@@ -20,11 +20,24 @@ fun task41(): Nothing = TODO(
 )
 
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z' }
+}
+
+fun <T, C : MutableCollection<T>> Collection<T>.partitionTo(
+        trueCollection: C,
+        falseCollection: C,
+        predicate: (T) -> Boolean)
+        : Pair<C, C> {
+    this.forEach {
+        if (predicate(it)) {
+            trueCollection.add(it)
+        } else {
+            falseCollection.add(it)
+        }
+    }
+    return trueCollection to falseCollection
 }
